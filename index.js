55e3906f4e87c7f46e4b5b11701f0e5833b55b8c
youzhen <yl301114@gmail.com>
2024-03-06 00:44:51 +0000

    換寫法

--- index.js
+++ index.js
@@ -1,20 +1,50 @@
-const boxes = document.querySelectorAll(".box"),
-    image = document.querySelector(".image");
+// const boxes = document.querySelectorAll(".box"),
+//     image = document.querySelector(".image");
 
-// console.log(boxes, image);
+// boxes.forEach(box => {
+//     box.addEventListener("dragover", (e) => {
+//         e.preventDefault();
+//         box.classList.add("hovered");
+//     });
 
-boxes.forEach(box => {
-    box.addEventListener("dragover", (e) => {
-        e.preventDefault();
-        // console.log("dragging");
-        box.classList.add("hovered");
-    });
+//     box.addEventListener("dragleave", (e) => {
+//         box.classList.remove("hovered");
+//     });
 
-    box.addEventListener("dragleave", (e) => {
-        box.classList.remove("hovered");
-    });
+//     box.addEventListener("drop", (e) => {
+//         box.appendChild(image);
+//     });
+// })
 
-    box.addEventListener("drop", (e) => {
-        box.appendChild(image);
-    });
-})
+
+
+// var image = document.querySelector(".image");
+
+// function dragImage(img) {
+//     let imgTop = img.offsetTop;
+//     let imgLeft = img.offsetLeft;
+//     console.log("top:" + imgTop + ", left:" + imgLeft);
+// }
+
+// image.addEventListener('mousedown',function(){
+//     dragImage(image);
+// });
+// image.addEventListener('touchstart',function(){
+// });
+
+// image.addEventListener('mousedown',function(){
+// });
+// image.addEventListener('touchstart',function(){
+// });
+
+window.onload = function() {
+    var dropItem = document.getElementById("dropItem");
+
+    dropItem.addEventListener('touchmove', function(ev) {
+        console.log("touch!");
+        var touchLocation = ev.targetTouches[0];
+
+        dropItem.style.left = touchLocation.pageX + 'px';
+        dropItem.style.top = touchLocation.pageY + 'px';
+    })
+}
